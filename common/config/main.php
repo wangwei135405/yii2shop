39acfd62c5e67028588880c496415198cf5d79e8
wang <wang@qq.com>
2017-06-16 14:33:07 +0800

    6/16

--- common/config/main.php
+++ common/config/main.php
@@ -4,6 +4,9 @@ return [
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
-        ],
+            ],
+        'authManager'=>[
+                'class'=>\yii\rbac\DbManager::className(),
+            ]
     ],
 ];
